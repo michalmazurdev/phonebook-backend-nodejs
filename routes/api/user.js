f667dd426d8cc7159e4e1123dc50d8c6c83c798b
michalmazurdev <m.mazur40@gmail.com>
2023-09-12 02:49:53 +0200

    login added

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 const Joi = require("joi");
 const User = require("../../service/schemas/userSchema");
+const secret = process.env.JWT_SECRET_KEY;
 
-const signUpBodyScheme = Joi.object({
+const bodyScheme = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
 
 router.post("/signup", async (req, res) => {
-  const validatedBody = signUpBodyScheme.validate(req.body);
+  const validatedBody = bodyScheme.validate(req.body);
   if (validatedBody.error?.details.length > 0) {
     return res.status(400).json({ message: validatedBody.error.message });
   }
@@ -39,6 +41,39 @@ router.post("/signup", async (req, res) => {
     console.error(error);
     return res.status(500).json({ message: "Server Error" });
   }
+});
+
+router.post("/login", async (req, res) => {
+  const validatedBody = bodyScheme.validate(req.body);
+  if (validatedBody.error?.details.length > 0) {
+    return res.status(400).json({ message: validatedBody.error.message });
+  }
+
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user || !user.validPassword(password)) {
+    return res.status(401).json({
+      status: "Unauthorized",
+      code: 400,
+      message: "Email or password is wrong",
+      data: "Bad request",
+    });
+  }
+
+  const payload = {
+    id: user.id,
+    username: user.username,
+  };
+
+  const token = jwt.sign(payload, secret, { expiresIn: "1h" });
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      token,
+    },
+  });
 });
 
 module.exports = router;
