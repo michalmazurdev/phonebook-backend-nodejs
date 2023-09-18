ba9f0c39bf4e325762f4a9c5a33694df7dc3d056
michalmazurdev <m.mazur40@gmail.com>
2023-09-18 03:18:10 +0200

    muilter middleware added, controler to be done

---
+++ controllers/user/updateAvatar.js
@@ -0,0 +1,5 @@
+const updateAvatar = (req, res) => {
+  console.log(req.file);
+  res.status(200);
+};
+module.exports = updateAvatar;
--- routes/api/user.js
+++ routes/api/user.js
@@ -5,7 +5,9 @@ const signup = require("../../controllers/user/signup");
 const login = require("../../controllers/user/login");
 const logout = require("../../controllers/user/logout");
 const getCurrentUser = require("../../controllers/user/getCurrentUser");
+const updateAvatar = require("../../controllers/user/updateAvatar");
 const authenticate = require("../../middleware/authenticate");
+const uploadAvatar = require("../../middleware/uploadAvatar");
 
 // @ POST /api/users/signup
 router.post("/signup", signup);
@@ -18,5 +20,13 @@ router.post("/logout", authenticate, logout);
 
 // @ GET /api/users/current
 router.get("/current", authenticate, getCurrentUser);
+
+// @ PATCH /api/users/avatars
+router.patch(
+  "/avatars",
+  authenticate,
+  uploadAvatar.single("avatar"),
+  updateAvatar
+);
 
 module.exports = router;
